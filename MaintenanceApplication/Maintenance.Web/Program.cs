f45bb404635fb5cec4e889112f2b0d383743b612
SafwaanShayk <safwaanshaikh17@gmail.com>
2025-01-14 18:53:20 +0500

    add : web solution and ref

--- MaintenanceApplication/Maintenance.Web/Program.cs
+++ MaintenanceApplication/Maintenance.Web/Program.cs
@@ -1,8 +1,9 @@
+using API.DependancyContainer;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.RegistrationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
