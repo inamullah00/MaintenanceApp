9aeb59b613fbd202b2f0ccaca5e7f5edddd5a733
SafwaanShayk <safwaanshaikh17@gmail.com>
2025-02-04 21:02:49 +0500

    Added : country id in cleint entity

--- MaintenanceApplication/Domain/Entity/ClientEntities/Client.cs
+++ MaintenanceApplication/Domain/Entity/ClientEntities/Client.cs
@@ -1,21 +1,17 @@
 ﻿using Domain.Common;
 using Maintenance.Domain.Entity.Dashboard;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Maintenance.Domain.Entity.FreelancerEntities;
 
 namespace Maintenance.Domain.Entity.ClientEntities
 {
-    public class Client:BaseEntity
+    public class Client : BaseEntity
     {
         public string? FullName { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Password { get; set; }
-        public string? ConfirmPassword { get; set; }
-        public string? Country { get; set; }
+        public Guid? CountryId { get; set; }
+        public Country? Country { get; set; }
         public ICollection<Order> ClientOrders { get; set; } // Orders placed by the client
         public ICollection<Feedback> TotalProvidedFeedbacksByClient { get; set; } // Feedback given by Client
 
