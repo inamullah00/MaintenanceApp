f500b5a576aee301e168d17f73f958cafd3dba30
SafwaanShayk <safwaanshaikh17@gmail.com>
2025-02-18 18:14:10 +0500

    migration : settings entity

---
+++ MaintenanceApplication/Infrastructure/Configurations/SettingConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Maintenance.Domain.Entity.SettingEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Maintenance.Infrastructure.Configurations
+{
+    public class SettingConfiguration : IEntityTypeConfiguration<Setting>
+    {
+        public void Configure(EntityTypeBuilder<Setting> builder)
+        {
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Key).IsRequired().HasMaxLength(500);
+            builder.Property(a => a.Value).IsRequired();
+            builder.HasIndex(a => a.Key).IsUnique();
+        }
+    }
+}
